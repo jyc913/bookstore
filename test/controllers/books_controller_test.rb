80ddadbdf1194efa2152b8dfa54063b19e01e90f
jean_chen <chenyangjing@yahoo.com>
2015-04-08 17:00:45 -0700

    updated controller

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -4,11 +4,14 @@ class BooksControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_not_nil assigns(:books)
   end
 
   test "should get show" do
-    get :show
+    get :show, id: books(:ender).id
     assert_response :success
+    book = assigns(:book)
+    assert_not_nil book
+    assert_equal "Ender's Game", book.title
   end
-
 end
